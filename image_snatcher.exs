a38901bbe742d5e0354487ca43b5ccf293d90d59
Joshua Gato <joshuagato37@gmail.com>
2022-08-02 10:54:43 +0000

    Initial commit

---
+++ image_snatcher.exs
@@ -0,0 +1,22 @@
+matcher = ~r/\.(jpg|jpeg|gif|png|bmp)$/
+matched_files = File.ls! |> Enum.filter(&(Regex.match?(matcher, &1)))
+
+num_matched = Enum.count matched_files
+msg_end = case num_matched do
+  1 -> "file"
+  _ -> "files"
+end
+
+IO.puts "Matched #{num_matched} #{msg_end}"
+
+case File.mkdir ("./images") do
+  :ok -> IO.puts "./images directory successfully created"
+  {:error, _} -> IO.puts "Could not create ./images directory"
+end
+
+Enum.each matched_files, fn filename ->
+  case File.rename(filename, "./images/#{filename}") do
+    :ok -> IO.puts "#{filename} successfully move to images directory"
+    {_, _} -> IO.puts "Error moving to images directory"
+  end
+end
